6418917e413b13415f429adb8f264ccf99d6615f
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-08-24 05:50:01 +0900

    fix: Edit build.gradle.kts
    - Configure reactor and test libraries

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "1.9.0"
     id("org.jlleitschuh.gradle.ktlint") version "11.5.1"
+    id("io.spring.dependency-management") version "1.1.3"
 }
 
 group = "me.bossm0n5t3r"
@@ -10,8 +11,20 @@ repositories {
     mavenCentral()
 }
 
+dependencyManagement {
+    imports {
+        mavenBom("io.projectreactor:reactor-bom:2022.0.10")
+    }
+}
+
 dependencies {
+    implementation("io.projectreactor:reactor-core")
+    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+    implementation("io.reactivex.rxjava3:rxjava:3.1.6")
+
     testImplementation(kotlin("test"))
+    testImplementation("io.projectreactor:reactor-test")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
 }
 
 tasks.test {
