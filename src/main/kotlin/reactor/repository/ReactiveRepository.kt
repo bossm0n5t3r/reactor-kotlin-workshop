c6c675094ac257d283a951ddf71e8299e69c149d
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-08-26 00:49:41 +0900

    feat: Add ReactiveRepository

---
+++ src/main/kotlin/reactor/repository/ReactiveRepository.kt
@@ -0,0 +1,16 @@
+package reactor.repository
+
+import org.reactivestreams.Publisher
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+
+interface ReactiveRepository<T> {
+
+    fun save(publisher: Publisher<T>): Mono<Void>
+
+    fun findFirst(): Mono<T>
+
+    fun findAll(): Flux<T>
+
+    fun findById(id: String): Mono<T>
+}
