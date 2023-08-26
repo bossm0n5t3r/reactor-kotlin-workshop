8881b830a969e2c07dfb78a999057d7abf0ce476
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-08-26 16:23:01 +0900

    Part07. Errors

--- src/main/kotlin/reactor/Part07Errors.kt
+++ src/main/kotlin/reactor/Part07Errors.kt
@@ -1,5 +1,6 @@
 package reactor
 
+import reactor.core.Exceptions
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import reactor.domain.User
@@ -9,35 +10,32 @@ class Part07Errors {
 // ========================================================================================
 
     fun betterCallSaulForBogusMono(mono: Mono<User>): Mono<User> {
-        TODO(
-            "Return a Mono<User> containing User.SAUL " +
-                "when an error occurs in the input Mono, " +
-                "else do not change the input Mono.",
-        )
+        return mono
+            .onErrorResume { Mono.just(User.SAUL) }
     }
 
 // ========================================================================================
 
     fun betterCallSaulAndJesseForBogusFlux(flux: Flux<User>): Flux<User> {
-        TODO(
-            "Return a Flux<User> containing User.SAUL and User.JESSE " +
-                "when an error occurs in the input Flux, " +
-                "else do not change the input Flux.",
-        )
+        return flux
+            .onErrorResume { Flux.just(User.SAUL, User.JESSE) }
     }
 
 // ========================================================================================
 
     fun capitalizeMany(flux: Flux<User>): Flux<User> {
-        TODO(
-            "Implement a method that capitalizes each user of the incoming flux " +
-                "using the #capitalizeUser method and " +
-                "emits an error containing a GetOutOfHereException error",
-        )
+        return flux
+            .handle { user, sink ->
+                try {
+                    sink.next(capitalizeUser(user))
+                } catch (e: GetOutOfHereException) {
+                    sink.error(Exceptions.propagate(e))
+                }
+            }
     }
 
     @Throws(GetOutOfHereException::class)
-    fun capitalizeUser(user: User): User {
+    private fun capitalizeUser(user: User): User {
         if (user == User.SAUL) {
             throw GetOutOfHereException()
         }
