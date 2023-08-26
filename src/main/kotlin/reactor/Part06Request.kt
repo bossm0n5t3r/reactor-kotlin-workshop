03a381d8a988f51e44c22c4adc37a1892ce9a7fe
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-08-26 10:37:10 +0900

    Part06. Request

--- src/main/kotlin/reactor/Part06Request.kt
+++ src/main/kotlin/reactor/Part06Request.kt
@@ -21,18 +21,29 @@ class Part06Request {
 // ========================================================================================
 
     fun requestOneExpectSkylerThenRequestOneExpectJesse(flux: Flux<User>): StepVerifier {
-        TODO("Create a StepVerifier that initially requests 1 value and expects User.SKYLER then requests another value and expects User.JESSE then stops verifying by cancelling the source")
+        TODO(
+            "Create a StepVerifier that " +
+                "initially requests 1 value and expects User.SKYLER " +
+                "then requests another value and expects User.JESSE " +
+                "then stops verifying by cancelling the source",
+        )
     }
 
 // ========================================================================================
 
     fun fluxWithLog(): Flux<User> {
-        TODO("Return a Flux with all users stored in the repository that prints automatically logs for all Reactive Streams signals")
+        TODO(
+            "Return a Flux with all users stored in the repository that " +
+                "prints automatically logs for all Reactive Streams signals",
+        )
     }
 
 // ========================================================================================
 
     fun fluxWithDoOnPrintln(): Flux<User> {
-        TODO("Return a Flux with all users stored in the repository that prints \"Starring:\" at first, \"firstname lastname\" for all values and \"The end!\" on complete")
+        TODO(
+            "Return a Flux with all users stored in the repository that " +
+                "prints \"Starring:\" at first, \"firstname lastname\" for all values and \"The end!\" on complete",
+        )
     }
 }
