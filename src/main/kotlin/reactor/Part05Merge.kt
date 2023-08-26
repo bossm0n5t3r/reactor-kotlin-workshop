1177b2ce2313cfe5ca923c7320da8da12353a6a5
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-08-26 11:43:31 +0900

    Part05. Merge

--- src/main/kotlin/reactor/Part05Merge.kt
+++ src/main/kotlin/reactor/Part05Merge.kt
@@ -12,18 +12,18 @@ class Part05Merge {
 // ========================================================================================
 
     fun mergeFluxWithInterleave(flux1: Flux<User>, flux2: Flux<User>): Flux<User> {
-        TODO("Merge flux1 and flux2 values with interleave")
+        return flux1.mergeWith(flux2)
     }
 
 // ========================================================================================
 
     fun mergeFluxWithNoInterleave(flux1: Flux<User>, flux2: Flux<User>): Flux<User> {
-        TODO("Merge flux1 and flux2 values with no interleave (flux1 values and then flux2 values)")
+        return flux1.concatWith(flux2)
     }
 
 // ========================================================================================
 
     fun createFluxFromMultipleMono(mono1: Mono<User>, mono2: Mono<User>): Flux<User> {
-        TODO("Create a Flux containing the value of mono1 then the value of mono2")
+        return Flux.concat(mono1, mono2)
     }
 }
