97745a74ab73df09d973cd58d7e2c87a642fa76a
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-08-26 10:39:40 +0900

    Part07. Errors

--- src/main/kotlin/reactor/Part07Errors.kt
+++ src/main/kotlin/reactor/Part07Errors.kt
@@ -9,19 +9,31 @@ class Part07Errors {
 // ========================================================================================
 
     fun betterCallSaulForBogusMono(mono: Mono<User>): Mono<User> {
-        TODO("Return a Mono<User> containing User.SAUL when an error occurs in the input Mono, else do not change the input Mono.")
+        TODO(
+            "Return a Mono<User> containing User.SAUL " +
+                "when an error occurs in the input Mono, " +
+                "else do not change the input Mono.",
+        )
     }
 
 // ========================================================================================
 
     fun betterCallSaulAndJesseForBogusFlux(flux: Flux<User>): Flux<User> {
-        TODO("Return a Flux<User> containing User.SAUL and User.JESSE when an error occurs in the input Flux, else do not change the input Flux.")
+        TODO(
+            "Return a Flux<User> containing User.SAUL and User.JESSE " +
+                "when an error occurs in the input Flux, " +
+                "else do not change the input Flux.",
+        )
     }
 
 // ========================================================================================
 
     fun capitalizeMany(flux: Flux<User>): Flux<User> {
-        TODO("Implement a method that capitalizes each user of the incoming flux using the #capitalizeUser method and emits an error containing a GetOutOfHereException error")
+        TODO(
+            "Implement a method that capitalizes each user of the incoming flux " +
+                "using the #capitalizeUser method and " +
+                "emits an error containing a GetOutOfHereException error",
+        )
     }
 
     @Throws(GetOutOfHereException::class)
